870c9742cb0d9eb163773590fe5152a3fea269bd
Jan Blasko <honza@lucien144.net>
2018-08-15 09:41:21 +1000

    Allow to match reg. expressions too.
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- AdminerColors.php
+++ AdminerColors.php
@@ -19,6 +19,13 @@ class AdminerColors
 		$color = $this->colors[$_SERVER['SERVER_NAME']] ?? self::DEFAULT_COLOR;
 		$color = $this->colors[$_SERVER['SERVER_ADDR']] ?? self::DEFAULT_COLOR;
 
+		foreach ($this->colors as $pattern => $value) {
+			$grep = preg_grep($pattern, [$_GET['server'], $_GET['pgsql'], $_SERVER['SERVER_NAME'], $_SERVER['SERVER_ADDR']]);
+			if (!empty($grep)) {
+				$color = $value;
+			}
+		}
+
 		echo '<style>
 			#menu { border-left: 1em solid ' . $color . '; min-height: 100%; }
 			#menu h1 a { color: ' . $color . ' }
